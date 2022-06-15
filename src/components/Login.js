76de20b182190080f208f0447bf49eb3c968b655
gyash3456 <gyash3456@gmail.com>
2022-06-15 14:51:06 +0530

    login page started

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './login.css'
+import pic from './../assets/mlelogo.png'
+
+function Login() {
+  return (
+    <div className='dialoguebox-wrapper'>
+        <div className='dialoguebox'><img src={pic}/><h3 className='dialoguebox-label'>Employee Portal Login</h3></div>
+
+      
+    </div>
+  )
+}
+
+export default Login
