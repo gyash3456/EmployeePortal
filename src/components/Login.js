f35ba7dd68966e0155376afa11b556e54e021a2a
gyash3456 <gyash3456@gmail.com>
2022-06-20 16:00:58 +0530

    ui-completed validations remaining

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,14 +13,17 @@ function Login() {
         </div>
         
         <div class="input-icons">
-            <i class="fa fa-user icon"></i>
+          <div class="align-middle">
+          <i class=" align-middle fa fa-user icon fa-lg mt-2"></i>
+          </div>
+            
             <input class="input-field" placeholder='Enter your User Id' type="text"></input>
             <br/>
-            <i class="fa fa-lock icon"></i>
+            <i class="fa fa-lock icon fa-lg mt-2"></i>
             <input class='input-field' placeholder='Enter your password'></input>
         </div>
         
-        <div className='forget'>Forgot Password?</div>
+        <div className='forget'><u>Forgot Password?</u></div>
         <div><button type="button" class="btn btn-primary">Login</button></div>
         </div>
         
